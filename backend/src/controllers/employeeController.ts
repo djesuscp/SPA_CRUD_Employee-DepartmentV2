1c698d49515d5afb6335b1494e282ac09d87a110
djesuscp <djesuscruzpomares@gmail.com>
2025-05-24 22:23:26 +0200

    Emergency save dated: 2025-05-24 22:23:26

--- backend/src/controllers/employeeController.ts
+++ backend/src/controllers/employeeController.ts
@@ -8,65 +8,56 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 
-// GET todos los empleados
+// Check admin. DONE
+const checkAdmin = (login: string | undefined) => {
+  if (login === 'admin') return true;
+  else return false;
+}
+
+// GET all employees. DONE
 export const getEmployees = async (_req: Request, res: Response) => {
   const employees = await prisma.employee.findMany();
   if (employees.length === 0) return res.status(404).json({ message: 'There are no employees yet.' });
-  res.json(employees);
+  else res.json(employees);
 };
 
-// GET empleado por DNI
+// GET employee by ID. DONE
 export const getEmployeeById = async (req: Request, res: Response) => {
   const { id } = req.params;
-
   try {
-    if (req.userLogin !== 'admin') {
+    if (!checkAdmin(req.userLogin)) {
       const employee = await prisma.employee.findUnique({ where: { login: req.userLogin } });
-      if (!employee || employee.id !== id) {
-        return res.status(403).json({ message: 'Access denied' });
-      }
+      if (!employee || employee.id !== id) return res.status(403).json({ message: 'Access denied' });
     }
-
     const employee = await prisma.employee.findUnique({ where: { id } });
     if (!employee) return res.status(404).json({ message: 'Employee not found' });
-
     res.json(employee);
   } catch (err) {
     res.status(500).json({ error: 'Error retrieving employee', detail: err });
   }
 };
 
-// PUT actualizar empleado
+// UPDATE employee. DONE
 export const updateEmployee = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { fullName, login, password, departmentId } = req.body;
 
-  if (!fullName || !login || !password || !departmentId) {
-    return res.status(400).json({ message: 'Some data is missing in the request body. Check it again, please.' });
-  }
+  if (!fullName || !login || !password || !departmentId) return res.status(400).json({ message: 'Some data is missing in the request body. Check it again, please.' });
 
-  if (fullName == 'admin' || login == 'admin') {
-    return res.status(409).json({ message: 'User already exists.' });
-  }
+  if (checkAdmin(login)) return res.status(409).json({ message: 'User already exists.' });
 
   try {
     const existing = await prisma.employee.findUnique({ where: { id } });
-    if (!existing) {
-      return res.status(404).json({ message: 'Employee does not exist.' });
-    }
-
+    if (!existing) return res.status(404).json({ message: 'Employee does not exist.' });
     const department = await prisma.department.findUnique({ where: { id: departmentId } });
-    if (!department) {
-      return res.status(404).json({ message: 'Department does not exist.' });
-    }
-    // Prepara el objeto de datos para la actualización
+    if (!department) return res.status(404).json({ message: 'Department does not exist.' });
     const updateData: any = {
       fullName,
       login,
       departmentId,
     };
 
-    // Si el campo password viene definido, lo encriptamos
+    // Password encrypt during update.
     if (password) {
       const hashedPassword = await bcrypt.hash(password, 10);
       updateData.password = hashedPassword;
@@ -76,10 +67,9 @@ export const updateEmployee = async (req: Request, res: Response) => {
       where: { id },
       data: updateData,
     });
-
-    // No devolvemos la contraseña
-    const { password: _, ...employeeWithoutPassword } = updatedEmployee;
-    res.json({ message: 'Employee successfully updated.' });
+     res.json({ message: 'Employee successfully updated.' });
+    // These 2 lines return the employee without the password.
+    //const { password: _, ...employeeWithoutPassword } = updatedEmployee;
     //res.json(employeeWithoutPassword);
   } catch (error) {
     console.error("Error updating employee:", error);
@@ -87,72 +77,58 @@ export const updateEmployee = async (req: Request, res: Response) => {
   }
 };
 
-// DELETE eliminar empleado
+// DELETE employee. DONE
 export const deleteEmployee = async (req: Request, res: Response) => {
   const { id } = req.params;
-
-  if(checkAdmin(id, '', '')) res.status(409).json({ message: "You are not able to delete Admin user from this app." });
-
-  const employee = await prisma.employee.findUnique({ where: { id } });
-
-  if (!employee) return res.status(404).json({ message: 'Employee does not exist.' });
-
-  await prisma.employee.delete({ where: { id } });
-  res.json({ message: 'Employee successfully deleted.' });
+  try {
+    const employee = await prisma.employee.findUnique({ where: { id }, select: { login: true}, });
+    if (!employee) return res.status(404).json({ message: 'Employee does not exist.' });
+    if (checkAdmin(employee.login)) return res.status(403).json({ message: 'Cannot delete the admin user.' });
+    await prisma.employee.delete({ where: { id } });
+    res.json({ message: 'Employee successfully deleted.' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error. Something went wrong trying to delete an employee.' });
+  }
 };
 
+// REGISTER or CREATE employee. DONE
 export const registerEmployee = async (req: Request, res: Response) => {
   const { id, fullName, login, password, departmentId } = req.body;
+  if (!id || !fullName || !login || !password || !departmentId) return res.status(400).json({ message: 'Missing fields in request body' });
+  if (checkAdmin(login)) return res.status(409).json({ message: 'User already exists.' });
 
-  if (!id || !fullName || !login || !password || !departmentId) {
-    return res.status(400).json({ message: 'Missing fields in request body' });
-  }
-
-  if(checkAdmin(id, fullName, login)) return res.status(409).json({ message: 'User admin already exists.' });
-
-  // if (fullName == 'admin' || login == 'admin') {
-  //   return res.status(409).json({ message: 'User already exists.' });
-  // }
-
-  // Verificar si ya existe un empleado con el mismo login
+  // Verify existing login.
   const exists = await prisma.employee.findUnique({ where: { login } });
-  if (exists) {
-    return res.status(400).json({ message: 'Employee with this login already exists' });
-  }
-
+  if (exists) return res.status(400).json({ message: 'Employee with this login already exists' });
   try {
-    // Encriptar contraseña
+    // Encrypt password.
     const hashedPassword = await bcrypt.hash(password, 10);
-    const employee = await prisma.employee.create({
-      data: { id, fullName, login, password: hashedPassword, departmentId }
-    });
+    const employee = await prisma.employee.create({ data: { id, fullName, login, password: hashedPassword, departmentId }});
     res.status(201).json(employee);
   } catch (error) {
     res.status(400).json({ error: 'Registration failed', detail: error });
   }
 };
 
+// LOGIN employee.
 export const login = async (req: Request, res: Response) => {
   const { login, password } = req.body;
   try {
-    if (login === 'admin') {
+    if (checkAdmin(login)) {
       const isMatch = await bcrypt.compare(password, process.env.ADMIN_PASSWORD as string);
       if (!isMatch) return res.status(401).json({ message: 'Incorrect password' });
       const token = jwt.sign({ login }, process.env.JWT_SECRET as string);
       return res.json({ token });
     }
-
     const employee = await prisma.employee.findUnique({ where: { login } });
     if (!employee) return res.status(404).json({ message: 'User not found' });
-
     const validPassword = await bcrypt.compare(password, employee.password);
     if (!validPassword) return res.status(401).json({ message: 'Incorrect password' });
 
-    // En tu controlador de login
+    // Generate token.
     const token = jwt.sign({ login: employee.login }, process.env.JWT_SECRET!, {
       expiresIn: '1h',
     });
-
     res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ error: 'Login failed', detail: err });
--- backend/src/routes/employeeRoutes.ts
+++ backend/src/routes/employeeRoutes.ts
@@ -4,16 +4,15 @@ import { authenticateToken, requireAdmin } from '../middlewares/authentication';
 
 const router = express.Router();
 
-// Registro y login sin token
+// Register and login.
 router.post('/employee/register', registerEmployee);
 router.post('/login', login);
 
-// Solo empleado logueado.
+// Admin can see all of them, while signed employee can see only its own data.
 router.get('/employee/:id', authenticateToken, getEmployeeById);
 
-// CRUD de empleados solo admin.
+// CRUD only for admin.
 router.get('/employee/', authenticateToken, requireAdmin, getEmployees);
-router.get('/employee/:id', authenticateToken, requireAdmin, getEmployeeById);
 router.put('/employee/:id', authenticateToken, requireAdmin, updateEmployee);
 router.delete('/employee/:id', authenticateToken, requireAdmin, deleteEmployee);
 
